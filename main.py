0aa5a919d9d9e28f2057a6ce5d16d12d5732a674
IlyaRyzhenkov <ilya.rigenkov2@gmail.com>
2019-12-16 13:04:02 +0500

    windows error

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import sys
 import Program
 import SocketAPI
 
 
 if __name__ == '__main__':
+    if sys.platform == 'win32':
+        sys.stderr.write('Windows don\'t supported')
+        exit(1)
+
     sock = SocketAPI.SocketAPI()
     program = Program.Program(sock)
     program.run()
-
